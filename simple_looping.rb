b600034a6d67be2208250990377f3139004799b1
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-06 16:42:54 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -42,7 +42,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-range=1..10
+range=1..number_of_times
 for nums in range
 phrase = "Welcome to Flatiron School's Web Development Course!"
 puts phrase
